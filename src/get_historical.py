b9ed40512f05fb4a27a05bf85db9990ac726bf70
kosmobiker <kosmobiker@gmail.com>
2021-12-12 20:37:02 +0100

    fixed historical upload

--- src/get_historical.py
+++ src/get_historical.py
@@ -9,20 +9,23 @@ import os
 import random
 import time
 import requests
-from utils.logger import setup_applevel_logger
 import pandas as pd
-from datetime import datetime 
-from utils.utils import read_config, simple_request, check_limit
+from datetime import datetime
+import sys; sys.path.insert(0, "utils") 
+from utils.read_config import read_config
+from utils.funcs import check_limit, simple_request
 from utils.logger import setup_applevel_logger
 
-CONFIG_PATH = "/home/vlad/master_project/config/cofig.yaml"
+ROOT_DIR = os.path.dirname(__file__)
+CONFIG_PATH = os.path.abspath(os.path.join(ROOT_DIR, '..', 'config/config.yaml'))
+TODAY = datetime.today().strftime("%Y%m%d")
 
 configs_dict = read_config(CONFIG_PATH)
 BASE_URL = configs_dict["baseUrl"]
 PATH_TO_SAVE = configs_dict["path_to_save_historical_data"]
+PATH_TO_LOGS = configs_dict["path_to_save_logs"]
 
-
-log = setup_applevel_logger(file_name = '../logs/my_debug.log')
+log = setup_applevel_logger(file_name = PATH_TO_LOGS + "logging_{}".format(TODAY))
 
 
 def get_historical_data(coin: str):
@@ -32,13 +35,12 @@ def get_historical_data(coin: str):
         res = requests.get(req).json()
         return res
     except:
-        log.error("Didn't fetch {}. Will try later".format(coin))
+        pass
 
 def get_all_historical():
-    print(datetime.now())
-    log.info('start!')
+    log.info('Starting to upload historical data for cryptocurrencies!')
     full_list = simple_request('coins/list')
-    names_of_coins = [item['id'] for item in full_list][:200]
+    names_of_coins = [item['id'] for item in full_list][:200] # for debug purposes
     while len(names_of_coins) > 0:
         try: 
             coin = names_of_coins[0]
@@ -49,5 +51,7 @@ def get_all_historical():
             log.debug("{} is uploaded".format(coin))
         except:
             pass
+    log.info('Historical data for cryptocurrency is uploaded')
+
 if __name__ == '__main__':
     get_all_historical()
