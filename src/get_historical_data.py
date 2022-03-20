5a27078976a7efba315cf55df40245f19dc93434
u.darhevich <u.darhevich@godeltech.com>
2022-03-20 16:11:27 +0100

    2022-03-20

--- src/get_historical_data.py
+++ src/get_historical_data.py
@@ -19,8 +19,6 @@ CONFIG_PATH = os.path.abspath(os.path.join(ROOT_DIR, '..', 'config', 'config.tom
 
 config = read_toml_config(CONFIG_PATH)
 API_ENDPOINT = URL(config['dev']['api_endpoint'])
-# PATH_COIN_LIST = os.path.abspath(os.path.join(ROOT_DIR, '..', config['dev']['path_coin_list']))
-# PATH_RAW_DATA = os.path.abspath(os.path.join(ROOT_DIR, '..', config['dev']['path_raw_data']))
 PATH_COIN_LIST = config['dev']['path_coin_list']
 PATH_RAW_DATA = config['dev']['path_raw_data']
 PATH_TO_LOGS = os.path.abspath(os.path.join(ROOT_DIR, '..', config['dev']['path_to_logs']))
@@ -39,6 +37,14 @@ def save_raw_json(data, coin: str):
     with open(PATH_RAW_DATA + '/{}_historical_prices.json'.format(coin), 'w') as f:
         json.dump(data, f)
 
+def lambda_get_data(func_name: str, params: dict, client):
+    response = client.invoke(
+            FunctionName=func_name,
+            InvocationType='Event',
+            Payload=json.dumps(params)
+        )
+    return response
+
 if __name__ == "__main__":
     #     if not os.path.exists(PATH_RAW_DATA):
     #         os.makedirs(PATH_RAW_DATA)
@@ -54,18 +60,31 @@ if __name__ == "__main__":
     # else:
     #session = boto3.Session(profile_name='default')
     session = boto3.Session(profile_name=AWS_PROFILE)
+    lambda_client = session.client('lambda')
     path_list_of_coins = f"s3://{AWS_BUCKET}/{PATH_COIN_LIST}/list_of_coins.csv"
-    list_of_coins = wr.s3.read_csv([path_list_of_coins], boto3_session=session)['id'].to_list()[20:30] #for test only
+    list_of_coins = wr.s3.read_csv([path_list_of_coins], boto3_session=session)['id'].to_list()[666:777] #for test only
+    params = {
+            "bucket": AWS_BUCKET,
+            "path_raw_data": PATH_RAW_DATA
+        }
+    func = 'Kosmo_Test_Lambda_Function'
+
     for coin in list_of_coins:
+        # try:
+        #     result = get_historical_data(coin)
+        #     path = f"{PATH_RAW_DATA}/{coin}_historical_prices.json"
+        #     upload_json_s3(result, session, AWS_BUCKET, path)
+        #     log.info(f"{coin} - raw json file was saved")
+        # except Exception as err:
+        #     log.error(f"{coin} - raw json file was not saved")
+        #     log.error(err)
         try:
-            result = get_historical_data(coin)
-            path = f"{PATH_RAW_DATA}/{coin}_historical_prices.json"
-            upload_json_s3(result, session, AWS_BUCKET, path)
+            params['coin'] = coin
+            lambda_get_data(func, params, lambda_client)
             log.info(f"{coin} - raw json file was saved")
         except Exception as err:
             log.error(f"{coin} - raw json file was not saved")
-            log.error(err)
-
+            log.error(err)        
             
 
 
